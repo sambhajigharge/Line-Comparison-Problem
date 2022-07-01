2d4caa9e01acddd2a1cfc3b58b96bdbec215c8cf
sushant <sgharge299@gmail.com>
2022-07-01 12:50:20 +0530

    [sushant]calculated the length of the line by using co-ordinates

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -1,7 +1,25 @@
 package LineComparison;
+import java.util.Scanner;
 
 public class LineComparisonComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to the Line Comparison Computation Program");
+
+        System.out.println("Calculate Length Between Two Point");
+        int x1,x2,y1,y2;
+        double Length;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter x1=");
+        x1=sc.nextInt();
+        System.out.println("enter y1=");
+        y1=sc.nextInt();
+        System.out.println("enter x2=");
+        x2=sc.nextInt();
+        System.out.println("enter y2=");
+        y2=sc.nextInt();
+        Length=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println("Length Between"+"("+x1+","+y1+"),"+"("+x2+","+y2+")="+Length);
     }
 }
+
+
